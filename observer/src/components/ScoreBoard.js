ebe255e16334a1a71339ad4d4e4f8f9ebec6e3df
NormalPlayerJSH <jsh010904@gmail.com>
2021-04-12 00:31:49 +0900

    팀 정보 UI 완성

--- observer/src/components/ScoreBoard.js
+++ observer/src/components/ScoreBoard.js
@@ -1,7 +1,41 @@
 function ScoreBoard({data}){
     return (
-        <div className="scoreboard">
-            {(data.common.endTime==-1)?0:parseInt((data.common.endTime-Date.now())/1000)}
+        <div className={`scoreboard`+((data.common.show.score)?' use-score':'')}>
+            <div className="board-team board-blue">
+                <div className="board-team-score">
+                    <div className="board-team-score-border">
+                        <div className="board-team-score-text">
+                            {data.blue[0].score}
+                        </div>
+                    </div>
+                </div>
+                <div className="board-team-name">
+                    <div className="board-team-name-text">
+                        {data.blue[0].name}
+                    </div>
+                </div>
+            </div>
+            <div className="board-time">
+                <div className="board-time-border">
+                    <div className="board-time-text">
+                        {(data.common.endTime==-1)?0:parseInt((data.common.endTime-Date.now())/1000)}
+                    </div>
+                </div>
+            </div>
+            <div className="board-team board-red">
+                <div className="board-team-score">
+                    <div className="board-team-score-border">
+                        <div className="board-team-score-text">
+                            {data.red[0].score}
+                        </div>
+                    </div>
+                </div>
+                <div className="board-team-name">
+                    <div className="board-team-name-text">
+                        {data.red[0].name}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
